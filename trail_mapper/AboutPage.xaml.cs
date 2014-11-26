14ddf26e4b592d846284bccfce394bfb60c3e0fc
Alex Martin <alexmartin9999@hotmail.com>
2014-11-25 21:25:49 -0800

    getting basic oneDrive sign-in example code to work

--- trail_mapper/AboutPage.xaml.cs
+++ trail_mapper/AboutPage.xaml.cs
@@ -3,14 +3,105 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Tasks;
 using System.Xml.Linq;
 using RateMyApp.Helpers;
+using Microsoft.Live;
+using Microsoft.Live.Controls;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Windows;
 
 namespace trail_mapper
 {
     public partial class AboutPage : PhoneApplicationPage
     {
+        private LiveConnectClient client;
+
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        private void loginButton_SessionChanged(object sender, LiveConnectSessionChangedEventArgs e)
+        {
+
+            if (e != null && e.Status == LiveConnectSessionStatus.Connected)
+            {
+                //the session status is connected so we need to set this session status to client
+                this.client = new LiveConnectClient(e.Session);
+                Upload.IsEnabled = true;
+            }
+            else
+            {
+                this.client = null;
+                Upload.IsEnabled = false;
+            }
+
+        }
+
+        private async void Connect_Click(object sender, EventArgs e)
+        {
+            bool connected = false;
+            try
+            {
+                var authClient = new LiveAuthClient("00000000441169AA");
+                LiveLoginResult result = await authClient.LoginAsync(new string[] { "wl.signin", "wl.skydrive", "wl.skydrive_update" });
+
+                if (result.Status == LiveConnectSessionStatus.Connected)
+                {
+                    connected = true;
+                    client = new LiveConnectClient(result.Session);
+                    //var meResult = await connectClient.GetAsync("me");
+                    //dynamic meData = meResult.Result;
+                    //updateUI(meData);
+                }
+            }
+            catch (LiveAuthException ex)
+            {
+                // Display an error message.
+            }
+            catch (LiveConnectException ex)
+            {
+                // Display an error message.
+            }
+
+            // Turn off the display of the connection button in the UI.
+            Upload.IsEnabled = connected;
+            ConnectButton.Visibility = connected ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private async void Upload_Click(object sender, EventArgs e)
+        {
+            if (client != null)
+            {
+                try
+                {
+                    string fileName = "sample.txt";
+                    IsolatedStorageFile myIsolatedStorage = IsolatedStorageFile.GetUserStoreForApplication();
+                    
+                    //deletes the file if it already exists
+                    if (myIsolatedStorage.FileExists(fileName))
+                    {
+                        myIsolatedStorage.DeleteFile(fileName);
+                    }
+                    
+                    //now we use a StreamWriter to write inputBox.Text to the file and save it to IsolatedStorage
+                    using (StreamWriter writeFile = new StreamWriter
+                    (new IsolatedStorageFileStream(fileName, FileMode.Create, FileAccess.Write, myIsolatedStorage)))
+                    {
+                        writeFile.WriteLine("Hello world");
+                        writeFile.Close();
+                    }
+                    IsolatedStorageFileStream isfs = myIsolatedStorage.OpenFile(fileName, FileMode.Open, FileAccess.Read);
+                    var res = await client.UploadAsync("me/skydrive", fileName, isfs, OverwriteOption.Overwrite);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please sign in with your Microsoft Account.");
+            }
         }
 
         public void TwitterButton_Click(object sender, EventArgs e)
